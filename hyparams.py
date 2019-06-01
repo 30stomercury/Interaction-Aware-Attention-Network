3a8e80de81b9ba6b7b66fd618468fcfb3c6c20e7
30stomercury <ff936tw@gmail.com>
2019-06-02 03:24:48 +0800

    updata train steps

--- hyparams.py
+++ hyparams.py
@@ -6,7 +6,7 @@ class hparams:
     ATTEN_SIZE = 16
     keep_proba = 0.9
     weight_decay = 1e-3
-    num_train_steps = 100 # 15000
+    num_train_steps = 1000 # 15000
     context_window_size = 1
     lr = 1e-4
     emo_train_file = 'emo_train.csv'
